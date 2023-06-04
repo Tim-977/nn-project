90d76825a50b1fb537266f2ffa46f15683e8eef5
Tim977 <gentim977@gmail.com>
2023-06-04 16:26:06 -0400

    Connected own hashing

--- data/users.py
+++ data/users.py
@@ -1,7 +1,7 @@
 import datetime
 import sqlalchemy
 from .db_session import SqlAlchemyBase
-from werkzeug.security import generate_password_hash, check_password_hash
+# from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
 
@@ -14,8 +14,8 @@ class User(SqlAlchemyBase, UserMixin):
     hashed_password = sqlalchemy.Column(sqlalchemy.String)
     created_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
 
-    def set_password(self, password):
-        self.hashed_password = generate_password_hash(password)
+    #def set_password(self, password):
+    #    self.hashed_password = generate_password_hash(password)
 
-    def check_password(self, password):
-        return check_password_hash(self.hashed_password, password)
+    #def check_password(self, password):
+    #    return check_password_hash(self.hashed_password, password)
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from functools import wraps
 from flask import Flask, redirect, render_template
 from flask_login import LoginManager, login_required, login_user, logout_user, current_user
 
+import hashing
 from data import db_session
 from data.users import User
 from forms.user import LoginForm, RegisterForm
@@ -71,7 +72,7 @@ def login():
     if form.validate_on_submit():
         db_sess = db_session.create_session()
         user = db_sess.query(User).filter(User.email == form.email.data).first()
-        if user and user.check_password(form.password.data):
+        if user and user.hashed_password == hashing.myhash(form.password.data):
             login_user(user, remember=form.remember_me.data)
             return redirect("/success")
         return render_template('login.html', message="Incorrect login or password", form=form)
@@ -93,7 +94,7 @@ def reqister():
         if db_sess.query(User).filter(User.email == form.email.data).first():
             return render_template('register.html', form=form, message="User already exists")
         user = User(name=form.name.data, email=form.email.data)
-        user.set_password(form.password.data)
+        user.hashed_password = hashing.myhash(form.password.data)
         db_sess.add(user)
         db_sess.commit()
         return redirect('/login')
