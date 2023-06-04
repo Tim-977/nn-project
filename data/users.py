0168642e86dfb0e356efd2ac858b831e677c6bbe
Tim977 <gentim977@gmail.com>
2023-06-04 18:36:44 -0400

    Minor update

--- data/users.py
+++ data/users.py
@@ -1,7 +1,6 @@
 import datetime
 import sqlalchemy
 from .db_session import SqlAlchemyBase
-# from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
 
@@ -13,9 +12,3 @@ class User(SqlAlchemyBase, UserMixin):
     email = sqlalchemy.Column(sqlalchemy.String, index=True, unique=True)
     hashed_password = sqlalchemy.Column(sqlalchemy.String)
     created_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
-
-    #def set_password(self, password):
-    #    self.hashed_password = generate_password_hash(password)
-
-    #def check_password(self, password):
-    #    return check_password_hash(self.hashed_password, password)
